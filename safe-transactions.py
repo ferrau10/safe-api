4d3fcdaa297d7ea843aedb93a48e6cee0673ff79
Aurelie Ferron <60011303+ferrau10@users.noreply.github.com>
2022-12-14 22:14:43 +0100

    Update safe-transactions.py

--- safe-transactions.py
+++ safe-transactions.py
@@ -3,16 +3,17 @@ import requests
 url = 'https://safe-transaction-mainnet.safe.global/api/v1/safes/0xBbA4C8eB57DF16c4CfAbe4e9A3Ab697A3e0C65D8/multisig-transactions/'
 r = requests.get(url)
 data =  r.json()
+
 wc_tx_all = 0
 
-while data['next']:  
+while True:  
     
-    for x in data['results']:
-        if isinstance(x['origin'], str) == True and "WalletConnect" in x['origin']: 
-            wc_tx_all += 1
+    wc_tx_all = wc_tx_all + len([x for x in data['results'] if x['origin'] and "WalletConnect" in x['origin']])     
     
+    if data['next'] is None:    
+        break
         
     r = requests.get(data['next'])
     data =  r.json()
     
-print(f'{wc_tx_all} WalletConnect transactions were made with this Safe')
+print(f'{wc_tx_all} WalletConnect transactions were made with this Safe') 
